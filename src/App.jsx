59b2c874b7282b67db76d033966f5e2b6779b046
Fred Nobre <frednobre18@gmail.com>
2024-01-18 10:04:54 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import s from "./style.module.css";
 import "./global.css";
 import { apiPokemon } from "./api/pokemon-api";
 import { PokemonList } from "./components/PokemonLIst/PokemonList";
-import { Card } from "./components/PokemonLIst/Card/Card";
+import { Logo } from "./components/Logo/Logo";
+//"//<a href="https://www.flaticon.com/fr/icones-gratuites/pokemon" title="pokémon icônes">Pokémon icônes créées par Nikita Golubev - Flaticon</a>
 export const App = () => {
   const [pokemonCollection, setPokemonCollection] = useState([]);
 
@@ -25,17 +26,18 @@ export const App = () => {
   console.log(pokemonCollection);
   return (
     <>
-    <div className={s.firstContainer}>
-
-      <div className={s.container}>
-        <div className={s.smallComtainer}>
-          <div>logo</div>
-          <div>title</div>
-          <div>searchBar</div>
+      <div className={s.firstContainer}>
+        <div className={s.container}>
+          <div className={s.smallComtainer}>
+            <div><Logo/></div>
+        
+            <div>title</div>
+            <div>searchBar</div>
+          </div>
         </div>
+        <PokemonList PokemonList={pokemonCollection} />\
       </div>
-        <PokemonList PokemonList={pokemonCollection} />
-    </div>
+          
     </>
   );
 };
