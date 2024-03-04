22de9c704f581ec5ffe30577f2aab447f1e5bf48
Fred Nobre <frednobre18@gmail.com>
2024-03-04 15:15:40 +0100

    feat: :sparkles: sort Pkmn by type wip

--- src/components/PokemonLIst/PokemonList.jsx
+++ src/components/PokemonLIst/PokemonList.jsx
@@ -1,25 +1,57 @@
 import React from "react";
 import s from "./style.module.css";
 import { Card } from "../Card/Card";
-export const PokemonList = ({ pokemonList }) => {
+export const PokemonList = ({ pokemonList, currentType }) => {
+  const simplifiedPokemonObject = pokemonList.map((pokemon) => {
+    return {
+      id: pokemon.id,
+      pokedexId: pokemon.pokedexId,
+      name: pokemon.name,
+      image: pokemon.image,
+      stats: pokemon.stats,
+      type: pokemon.apiTypes[0]
+    };
+  });
+
+  // tester pour la prochaine fois mapper dans le simplifiedPokemonObject
   return (
     <>
-      <div className={s.container}>
-        <div className={s.box}>
-          {pokemonList.map((pokemonItem) => {
-            return (
-              <div key={pokemonItem.id}>
-                <Card
-                  pokemonItem={pokemonItem}
-                  img={pokemonItem.image}
-                  type={pokemonItem.apiTypes[0]}
-                  stats={pokemonItem.stats}
-                />
-              </div>
-            );
-          })}
+      {!currentType ? (
+        <div className={s.container}>
+          <div className={s.box}>
+            {simplifiedPokemonObject.map((pokemon) => {
+              return (
+                <div key={pokemon.id}>
+                  <Card
+                    pokemon={pokemon}
+                    src={pokemon.image}
+                    type={pokemon.type}
+                    stats={pokemon.stats}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className={s.container}>
+          <div className={s.box}>
+            {pokemonList.map((pokemon) => {
+              if (pokemon.apiTypes.some((type) => type.name === currentType))
+                return (
+                  <div key={pokemon.id}>
+                    <Card
+                      pokemon={pokemon}
+                      src={pokemon.image}
+                      type={pokemon.apiTypes[0]}
+                      stats={pokemon.stats}
+                    />
+                  </div>
+                );
+            })}
+          </div>
+        </div>
+      )}
     </>
   );
 };
